cad650001bc37e108cf527d01ce0733c20a893e9
kattyolv <kattyolv@gmail.com>
2021-02-19 15:50:49 +0000

    Implements save method.

--- src/main/java/com/algaworks/billingmanagement/controller/AccountsReceivableController.java
+++ src/main/java/com/algaworks/billingmanagement/controller/AccountsReceivableController.java
@@ -1,13 +1,23 @@
 package com.algaworks.billingmanagement.controller;
 
+import com.algaworks.billingmanagement.model.AccountsReceivableModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/accounts-receivable")
 public class AccountsReceivableController {
 
-    @RequestMapping("/account-receivable/new")
+    @RequestMapping("/new")
     public String newAccountReceivable() {
+
+        return "Register";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String save(AccountsReceivableModel accountsReceivable) {
+
         return "Register";
     }
 
