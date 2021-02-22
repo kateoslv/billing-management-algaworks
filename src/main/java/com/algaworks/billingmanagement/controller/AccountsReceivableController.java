8aeafaec25b68071df16f68e48e7aff7ca918118
kattyolv <kattyolv@gmail.com>
2021-02-22 22:01:12 +0000

    Implements all accounts receivable status method.

--- src/main/java/com/algaworks/billingmanagement/controller/AccountsReceivableController.java
+++ src/main/java/com/algaworks/billingmanagement/controller/AccountsReceivableController.java
@@ -1,12 +1,17 @@
 package com.algaworks.billingmanagement.controller;
 
 import com.algaworks.billingmanagement.model.AccountReceivableModel;
+import com.algaworks.billingmanagement.model.enumerations.Status;
 import com.algaworks.billingmanagement.repository.AccountsReceivable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/accounts-receivable")
@@ -16,9 +21,11 @@ public class AccountsReceivableController {
     private AccountsReceivable accountsReceivable;
 
     @RequestMapping("/new")
-    public String newAccountReceivable() {
+    public ModelAndView newAccountReceivable() {
 
-        return "Register";
+        ModelAndView modelAndView = new ModelAndView("Register");
+
+        return modelAndView;
     }
 
     @RequestMapping(method = RequestMethod.POST)
@@ -30,6 +37,12 @@ public class AccountsReceivableController {
         modelAndView.addObject("message", "Saved successfully!");
 
         return modelAndView;
+    }
+
+    @ModelAttribute("statusAccountReceivable")
+    public List<Status> allAccountsReceivableStatus() {
+
+        return Arrays.asList(Status.values());
     }
 
 }
